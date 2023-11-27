7536c8b3ea47c9d0885761383183555b1a7823b0
Kate <ybelovan@gmail.com>
2023-11-27 22:28:27 +0300

    Добавьте файлы проекта.

---
+++ Gmap/Form1.cs
@@ -0,0 +1,30 @@
+﻿using GMap.NET.MapProviders;
+using System;
+using System.Windows.Forms;
+
+namespace Gmap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gMapControl1.MapProvider = GMapProviders.GoogleMap;
+            double lat = Convert.ToDouble(textBox2.Text);
+            double lng = Convert.ToDouble(textBox1.Text);
+            gMapControl1.Position = new GMap.NET.PointLatLng(lat, lng);
+            gMapControl1.MinZoom = 5;
+            gMapControl1.MaxZoom = 50;
+            gMapControl1.Zoom = 15;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
